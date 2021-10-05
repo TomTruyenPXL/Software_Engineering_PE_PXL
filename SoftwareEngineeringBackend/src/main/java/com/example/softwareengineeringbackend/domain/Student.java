d29025804845ad014f3058611a09f731ad6647ae
TomTruyenPXL <11900339@student.pxl.be>
2021-10-05 09:10:23 +0200

    Add student entity

---
+++ SoftwareEngineeringBackend/src/main/java/com/example/softwareengineeringbackend/domain/Student.java
@@ -0,0 +1,67 @@
+package com.example.softwareengineeringbackend.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+    @Id
+    private String guid;
+    private int number;
+    private String gender;
+    private String givenName;
+    private String surname;
+    private String emailAddress;
+
+    public Student() {
+        // JPA Only
+    }
+
+    public String getGuid() {
+        return guid;
+    }
+
+    public void setGuid(String guid) {
+        this.guid = guid;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getGivenName() {
+        return givenName;
+    }
+
+    public void setGivenName(String givenName) {
+        this.givenName = givenName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+}
